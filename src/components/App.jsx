ba1ad05cc838e1bf35cb2a5a6bbdb2c69d01fcbb
MunVlad <mvv2006@ukr.net>
2023-05-05 17:33:39 +0300

    second_task_done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,116 +1,91 @@
-import { nanoid } from 'nanoid';
 import React, { Component } from 'react';
-// import ContactForm from './ContactForm';
-// import ContactList from './ContactList';
-
-// class App extends Component {
-//   state = {
-//     contacts: [],
-//     name: '',
-//   };
-
-//   //  if (name.trim() === '') {
-//   //     return;
-//   //   }
-
-//   //   const newContact = { id: nanoid(), name };
-//   //   this.setState({ contacts: [...contacts, newContact], name: '' });
-//   // };
-
-//   render() {
-//     const { contacts, name } = this.state;
-//     return (
-//       <div
-//         style={{
-//           // height: '100vh',
-//           display: 'flex',
-//           justifyContent: 'center',
-//           alignItems: 'center',
-//           flexDirection: 'column',
-//           fontSize: 18,
-//           // textTransform: 'uppercase',
-//           color: '#010101',
-//         }}
-//       >
-//         <h1>Phonebook</h1>
-//       <form>
-//           <label htmlFor="">
-//             <input
-//               type="text"
-//               name="name"
-//               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//               required
-//             />
-//           </label>
-//         </form>
-//         <button type="submit">
-//           Add contact
-//         </button>
-//         <h2>Contacts</h2>
-//         <ul>
-//       {contacts.map(({ name, id }) => (
-//         <li key={id}>{name}</li>
-//       ))}
-//     </ul>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
+import shortid from 'shortid';
+import css from './App.module.css';
+import ContactList from './ContactList/ContactList';
+import ContactForm from './ContactForm';
+import Filter from './Filter';
 
 class App extends Component {
   state = {
-    contacts: [],
-    name: ''
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
   };
 
-  handleChange = event => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value });
-  };
+  addContact = ({ name, number }) => {
+    const normalizedName = name.toLowerCase();
 
-  handleSubmit = event => {
-    event.preventDefault();
+    let isAdded = false;
+    this.state.contacts.forEach(el => {
+      if (el.name.toLowerCase() === normalizedName) {
+        alert(`${name} is already in contacts`);
+        isAdded = true;
+      }
+    });
 
-    const { contacts, name } = this.state;
-
-    if (name.trim() === '') {
+    if (isAdded) {
       return;
     }
+    const contact = {
+      id: shortid.generate(),
+      name: name,
+      number: number,
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
 
-    const newContact = { id: nanoid(), name };
-    this.setState({ contacts: [...contacts, newContact], name: '' });
+  changeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  deleteContact = todoId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== todoId),
+    }));
   };
 
   render() {
-    const { contacts, name } = this.state;
+    const { contacts, filter } = this.state;
+    const visibleContacts = this.getVisibleContacts();
 
     return (
-      <div>
-        <h2>Phonebook</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Name
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={this.handleChange}
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </label>
-          <button type="submit">Add contact</button>
-        </form>
-        <h2>Contacts</h2>
-        <ul>
-          {contacts.map(({id, name}) => (
-            <li key={id}>{name}</li>
-          ))}
-        </ul>
+      <div
+        style={{
+          // height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          fontSize: 18,
+          // textTransform: 'uppercase',
+          color: '#010101',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+
+        <h2 className={css.titleContacts}>Contacts</h2>
+        <div className={css.allContacts}>All contacts: {contacts.length}</div>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
       </div>
     );
   }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,35 @@
-import React from "react";
-// import css from './ContactList.module.css';
-const contacts = [
-  { name: 'Rosie Simpson', id: 'id-1' },
-  { name: 'Hermione Kline', id: 'id-2' },
-  { name: 'Eden Clements', id: 'id-3' }
-];
+import css from './ContactList.module.css';
+import PropTypes from 'prop-types';
 
-function ContactList(props) {
-  return (
-    <ul>
-      {contacts.map(({ name, id }) => (
-        <li key={id}>{name}</li>
-      ))}
-    </ul>
-  );
-}
+const ContactList = ({ contacts, onDeleteContact }) => (
+  <ul className={css.list}>
+    {contacts.map(({ id, name, number }) => (
+      <li key={id} className={css.item}>
+        <p className={css.contact}>
+          {name}............
+          {number}
+        </p>
+        <button
+          className={css.btn}
+          type="submit"
+          onClick={() => onDeleteContact(id)}
+        >
+          Delete
+        </button>
+      </li>
+    ))}
+  </ul>
+);
 
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+};
 
 export default ContactList;
